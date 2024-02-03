955f18db597b94d1296b5260f7b31c7f8c8967ab
huix <19688861+helperfunc@users.noreply.github.com>
2024-02-03 23:07:41 +0800

    multiprocessing run the tasks with the ability to kill the processes

--- task/models.py
+++ task/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from enum import Enum
+
+class TaskStatus(Enum):
+    Processing = 'processing'
+    Processed = 'processed'
+    Failed = 'failed'
+    Completed = 'completed'
+    Kill = 'kill'
+    Killed = 'Killed'
 
 class Task(models.Model):
     function_path = models.CharField(max_length=255)
     input_data = models.TextField()
     result = models.TextField(null=True, blank=True)
-    status = models.CharField(max_length=50, default='processing')
+    status = models.CharField(max_length=50, default=TaskStatus.Processing.value)
+
--- task/urls.py
+++ task/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # 其他 URL 模式...
     path('post-data/', views.post_data, name='post_data'),
-    path('get-data/', views.get_data, name='get-data'),
+    path('get-data/', views.get_data, name='get_data'),
+    path('kill-process/', views.kill_process, name='kill_process')
 ]
